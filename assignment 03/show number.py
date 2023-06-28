07dda95252df7a7919fbee9e2022dc0d5e825843
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-06-28 11:05:34 +0330

    Create show number.py

---
+++ assignment 03/show number.py
@@ -0,0 +1,5 @@
+number = float(input("please enter a four digit number:\n"))
+three_digitnumber = int(number // 10)
+tow_digitnumber = int(number // 100)
+one_digitnumber = int(number // 1000)
+print(three_digitnumber, "\n", tow_digitnumber, "\n", one_digitnumber,)
