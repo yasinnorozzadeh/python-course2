6a812f58038aad688f70786029719a178f1ddefc
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-06-28 11:25:06 +0330

    Add files via upload

---
+++ assignment 03/Temperature_cal.py
@@ -0,0 +1,3 @@
+temperature_c = float(input("please enter the temperature in Celsius"))
+temperature_f = (temperature_c * 1.8) + 32
+print("temperature is\n","Celsius :" , temperature_c, "\n","Farenheit :", temperature_f)
