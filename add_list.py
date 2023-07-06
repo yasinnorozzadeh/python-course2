27c8acd5c544876f0616b05e97ebae6542366b04
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-06 21:38:46 +0330

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,5 @@
+nameslist = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
+nameslist.append(input("enter youre name:\n"))
+nameslist.append(input("enter youre name:\n"))
+nameslist.append(input("enter youre name:\n"))
+print(nameslist)
