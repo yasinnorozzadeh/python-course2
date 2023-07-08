976d16b2700b7ae2dc9ed8895500569987f4afa1
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-08 22:17:26 +0330

    Update queuing up.py

--- assignment 06/queuing up.py
+++ assignment 06/queuing up.py
@@ -1,7 +1,7 @@
 nameslist = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
-nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
+nameslist.insert(int(input("enter your number:\n"))-1 if int(input("enter your number:\n")) <= 6 else int(input("enter another number(range 1 to 6):\n")) , input("enter youre name:\n"))
 print(nameslist) # update names list
-nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n")) # insert(index name, name)
+nameslist.insert(int(input("enter your number:\n"))-1 if int(input("enter your number:\n")) <= 7 else int(input("enter another number(range 1 to 7):\n")) , input("enter youre name:\n")) # insert(index name, name)
 print(nameslist) # update names list
-nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
+nameslist.insert(int(input("enter your number:\n"))-1 if int(input("enter your number:\n")) <= 8 else int(input("enter another number(range 1 to 8):\n")) , input("enter youre name:\n"))
 print(nameslist)
