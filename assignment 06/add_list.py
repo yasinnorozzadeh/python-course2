6427170834c98373ee87c576845d621cff264492
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-08 15:18:03 +0330

    queuing up.py

--- assignment 06/add_list.py
+++
@@ -1,5 +0,0 @@
-nameslist = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
-nameslist.append(input("enter youre name:\n"))
-nameslist.append(input("enter youre name:\n"))
-nameslist.append(input("enter youre name:\n"))
-print(nameslist)
---
+++ assignment 06/queuing up.py
@@ -0,0 +1,5 @@
+nameslist = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
+nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
+nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
+nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
+print(nameslist)
