a1c0020ae5437cadb6c4613d5de33bf89c7b0ed4
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-08 19:08:38 +0330

    Update queuing up.py

--- assignment 06/queuing up.py
+++ assignment 06/queuing up.py
@@ -1,7 +1,7 @@
 nameslist = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
 nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
-print(nameslist)
-nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
-print(nameslist)
+print(nameslist) # update names list
+nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n")) # insert(index name, name)
+print(nameslist) # update names list
 nameslist.insert(int(input("enter your number:\n"))-1, input("enter youre name:\n"))
 print(nameslist)
