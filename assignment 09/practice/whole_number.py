19ee73a47401e17e386f3d1d2fda947cbcdb504d
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-16 09:41:21 +0330

    Update whole_number.py

--- assignment 09/practice/whole_number.py
+++ assignment 09/practice/whole_number.py
@@ -1,11 +1,11 @@
 
-number = abs(int(input("enter youre number:\n")))
+number = abs(int(input("enter your number:\n")))
 numbers = []
 for i in range(1, number):
     if (number % i) == 0:
         numbers.append(i)
 
 if sum(numbers) == number:
-    print("youre number is whole!")
+    print("your number is whole!")
 else:
-    print("youre number is not whole!")
+    print("your number is not whole!")
