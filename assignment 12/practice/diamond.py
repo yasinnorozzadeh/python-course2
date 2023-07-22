73d8f783060ae9b3f0d6e6060237e2aa76b3cf8d
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-23 00:09:38 +0330

    Update diamond.py

--- assignment 12/practice/diamond.py
+++ assignment 12/practice/diamond.py
@@ -1,9 +1,10 @@
 length = int(input("ernter length of diamond range(odd numbers): "))
-
+print("💙 🧡 💛 💜 💚 🤎 🤍 💘 💖 💝 💗 💓")
+char = input("enter youre character from imoji lists\n")
 for i in range(length):
-    print((length - i) * " ", end='')
-    print((i * 2 - 1) * '*')
+    print((length - i) * "  ", end='')
+    print((i * 2 - 1) * char)
 
 for i in range(length, 0, -1):
-    print((length - i) * " ", end='')
-    print((i * 2 - 1) * '*')
+    print((length - i) * "  ", end='')
+    print((i * 2 - 1) * char)
