689877b0f6c889ba3f52fb7b4d26c79d67513f59
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-22 16:37:01 +0330

    Update equation of the 2nd degree.py

--- assignment 11/practice/equation of the 2nd degree.py
+++ assignment 11/practice/equation of the 2nd degree.py
@@ -4,9 +4,16 @@ def e_2nd_d(a, b, c):
     # calculate the discriminant
     d = (b**2) - (4*a*c)
     # find two solutions
-    sol1 = (-b-math.sqrt(d))/(2*a)
-    sol2 = (-b+math.sqrt(d))/(2*a)
-    return sol1, sol2
+    if d > 0:
+        sol1 = (-b-math.sqrt(d))/(2*a)
+        sol2 = (-b+math.sqrt(d))/(2*a)
+        return sol1, sol2
+    elif d == 0:
+        sol3 = (-b / (a ** 2))
+        return sol3
+    else:
+        return None
+    
 a = int(input("enter a: "))
 b = int(input("enter b: "))
 c = int(input("enter c: "))
