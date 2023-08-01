5054a11f66c177f35c6891a849fdb0e029cfad73
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-08-01 20:28:59 +0330

    time.py

--- assignment 13/practice/time.py
+++ assignment 13/practice/time.py
@@ -1,5 +1,39 @@
 op = int(input("enter number of your operation:(1_sub, 2_sum):\n"))
-hour = int(input("enter hours: "))
+hour = int(input("enter hours: "))op = int(input("enter number of your operation:(1_sub, 2_sum):\n"))
+for i in range(1, 3):
+    hour = int(input("enter hours: "))
+    minut = int(input("enter minuts: "))
+    second = int(input("enter seconds: "))
+    if i == 1:
+        time1_dict = {"hour":hour, "minut":minut, "second":second}
+    else:
+        time2_dict = {"hour":hour, "minut":minut, "second":second}
+if op == 1:
+    h = time1_dict["hour"] - time2_dict["hour"]
+    m = time1_dict["minut"] - time2_dict["minut"]
+    s = time1_dict["second"] - time2_dict["second"]
+    while not(0 <= s <= 60 and 0 <= m <= 60):
+        if s < 0:
+            m -= 1
+            s += 60
+        if m < 0:
+            h -= 1
+            m += 60
+    print(str(h) + ":" + str(m)  + ":" + str(s))
+elif op == 2:
+    seconds = time1_dict["second"] + time2_dict["second"]
+    mins = time1_dict["minut"] + time2_dict["minut"]
+    hours = time1_dict["hour"] + time2_dict["hour"]
+    while not(0 <= seconds <= 60 and 0 <= mins <= 60):
+        if seconds > 60:
+            seconds -= ((seconds // 60) * 60)
+            mins += (seconds // 60)
+        if mins > 60:
+            mins -= ((mins // 60) * 60)
+            hours += (mins // 60)
+    print(str(hours) + ":" + str(mins)  + ":" + str(seconds))
+else:
+    print("your number is out of range\ntry again")
 minut = int(input("enter minuts: "))
 second = int(input("enter seconds: "))
 time1_dict = {"hour":hour,
