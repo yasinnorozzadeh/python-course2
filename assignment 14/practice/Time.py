24daa8920904a2be4fe8bd6c439707e103250b17
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-08-01 21:03:33 +0330

    Time.py

--- assignment 14/practice/Time.py
+++ assignment 14/practice/Time.py
@@ -1,34 +1,35 @@
 class Time:
-    # constructor
-    def __init__(self, hour, minut, second, hour2, minut2, second2, op):
-        # Properties
-        self.hour_s = hour + hour2
-        self.minut_s = minut + minut2
-        self.second_s = second + second2
-        self.hour_m = hour - hour2
-        self.minut_m = minut - minut2
-        self.second_m = second - second2
-        self.op = op
-    # Methods
+    def __init__(self, h, m, s, h2, m2, s2):
+        self.h = h
+        self.m = m
+        self.s = s
+        self.h2 = h2
+        self.m2 = m2
+        self.s2 = s2
     def sum(self):
-        while not(0 <= self.second_s <= 60 and 0 <= self.minut_s <= 60):
-            if self.second_s >= 60:
-                self.second_s -= ((self.second_s // 60) * 60)
-                self.minut_s += (self.second_s // 60)
-            if self.minut_s >= 60:
-                self.minut_s -= ((self.minut_s // 60) * 60)
-                self.hour_s += (self.minut_s // 60)
-        print(str(self.hour_s) + ":" + str(self.minut_s)  + ":" + str(self.second_s))
+        hour_s = self.h + self.h2
+        minut_s = self.m + self.m2
+        second_s = self.s + self.s2
+        while not(0 <= second_s <= 60 and 0 <= minut_s <= 60):
+            if second_s >= 60:
+                second_s -= ((second_s // 60) * 60)
+                minut_s += (second_s // 60)
+            if minut_s >= 60:
+                minut_s -= ((minut_s // 60) * 60)
+                hour_s += (minut_s // 60)
+        print(str(hour_s) + ":" + str(minut_s)  + ":" + str(second_s))
     def sub(self):
-            while not(0 <= self.second_m <= 60 and 0 <= self.minut_m <= 60):
-                if self.second_m < 0:
-                    self.minut_m -= 1
-                    self.second_m += 60
-                    if self.minut_m < 0:
-                        self.hour_m -= 1
-                        self.minut_m += 60
-            print(str(self.hour_m) + ":" + str(self.minut_m)  + ":" + str(self.second_m))
-
+        hour_m = self.h - self.h2
+        minut_m = self.m - self.m2
+        second_m = self.s - self.s2
+        while not(0 <= second_m <= 60 and 0 <= minut_m <= 60):
+            if second_m < 0:
+                minut_m -= 1
+                second_m += 60
+            if minut_m < 0:
+                hour_m -= 1
+                minut_m += 60
+        print(str(hour_m) + ":" + str(minut_m)  + ":" + str(second_m))
 t = input("enter time1:for exampel (20:45:30)\n")
 t2 = input("enter time2:for exampel (20:45:30)\n")
 op = int(input("1_sub\n2_sum\nenter number of your choice: "))
@@ -36,13 +37,8 @@ t = t.split(":")
 t = [eval(i) for i in t]
 t2 = t2.split(":")
 t2 = [eval(i) for i in t2]
-time = Time(t[0], t[1], t[2], t2[0], t2[1], t2[2], op)
+time = Time(t[0], t[1], t[2], t2[0], t2[1], t2[2])
 if op == 1:
     time.sub()
-elif op == 2:
-    time.sum()
 else:
-    print("youre numner is out of range\ntry again")
-
-
-    
+    time.sum()
