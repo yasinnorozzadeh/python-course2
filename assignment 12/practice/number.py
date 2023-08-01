6549c8755fe21b318aa1429f0636e3cb732aa904
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-08-01 20:02:12 +0330

    number.py

--- assignment 12/practice/number.py
+++ assignment 12/practice/number.py
@@ -1,14 +1,35 @@
-def num(number):
-    numbers_list = []
-    s = 0
-    for i in range (1, 4):
-        for j in str(number * i):
-            s += int(j)
-        numbers_list.append(str(s))
-        s = 0
-    number = " ".join(numbers_list)
-    number = number.replace(" ", "")
-    return number
+# def num(number):
+#     numbers_list = []
+#     s = 0
+#     for i in range (1, 4):
+#         for j in str(number * i):
+#             s += int(j)
+#         numbers_list.append(str(s))
+#         s = 0
+#     number = " ".join(numbers_list)
+#     number = number.replace(" ", "")
+#     return number
 
-number = int(input("enter youre number: "))
-print(num(number))
+# number = int(input("enter youre number: "))
+# print(num(number))
+
+
+
+# def number(num):
+#     n1 = num
+#     n2 = (num*10)+num
+#     n3 = (num*100)+(num*10)+num
+#     return n1 + n2 + n3
+# number_user = int(input("enter youre number: "))
+# print(number(number_user))
+
+
+
+def number(num):
+    num = str(num)
+    n1 = num
+    n2 = num+num
+    n3 = num+num+num
+    return int(n1) + int(n2) + int(n3)
+number_user = int(input("enter youre number: "))
+print(number(number_user))
